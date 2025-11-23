d6edd087b8a8c634407b6c2c67043ca9e708f87c
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-11-24 05:29:58 +0530

    create resume builder application initial commit

---
+++ src/main/java/dev/tharani/resumeBuilderApi/ResumeBuilderApiApplication.java
@@ -0,0 +1,13 @@
+package dev.tharani.resumeBuilderApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeBuilderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeBuilderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tharani/resumeBuilderApi/ResumeBuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tharani.resumeBuilderApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBuilderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
